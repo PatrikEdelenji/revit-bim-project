fdeb049acbcc6fa39d61c4363937c83972c49434
PatrikEdelenji <patrik.edelenji@gmail.com>
2026-06-01 17:45:49 +0200

    added C# addin

---
+++ revit-addin/RevitRoomExporter/ExportRoomsCommand.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace RevitRoomExporter
+{
+    [Transaction(TransactionMode.Manual)]
+    public class ExportRoomsCommand : IExternalCommand
+    {
+        public Result Execute(
+            ExternalCommandData commandData,
+            ref string message,
+            ElementSet elements)
+        {
+            TaskDialog.Show("Revit Room Exporter", "Add-in is working!");
+            return Result.Succeeded;
+        }
+    }
+}
